baff7e3e74d2286e9934aefbf4763310458aba0e
Thais Kusuki <thais.kusuki@gmail.com>
2018-02-12 00:14:21 -0200

    Adding new.html.erb view and passing the test 'Visitor register recipe successfully'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @receitas = Recipe.all
+    # destruir = Recipe.find(6)
+    # destruir.destroy
   end
 
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,37 @@
 class RecipesController < ApplicationController
   def show
-    @receita = Recipe.find(params[:id]) 
+    @receita = Recipe.find(params[:id])
+  end
+
+  def new
+    @nova_receita = Recipe.new
+  end
+
+  def create
+
+    recipe = params[:recipe]
+    title = recipe[:title]
+
+    title = recipe[:title]
+    recipe_type = recipe[:recipe_type]
+    cuisine = recipe[:cuisine]
+    difficulty = recipe[:difficulty]
+    cook_time = recipe[:cook_time]
+    ingredients = recipe[:ingredients]
+    method = recipe[:method]
+
+    receita = Recipe.new(
+                          title: title,
+                          recipe_type: recipe_type,
+                          cuisine: cuisine,
+                          difficulty: difficulty,
+                          cook_time: cook_time,
+                          ingredients: ingredients,
+                          method: method
+                        )
+    # puts receita
+    receita.save
+    redirect_to recipe_path(receita.id)
   end
 
 
