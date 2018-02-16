6ce7be8dfbc2369cb2a1e95cd8982146af0ad5ce
Thais Kusuki <thais.kusuki@gmail.com>
2018-02-16 11:25:44 -0200

    Adding a view new.html.erb to cuisine and passing the test 'user register cuisine and must fill in name'

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,6 +1,23 @@
 class CuisinesController < ApplicationController
   def show
     @cozinha = Cuisine.find(params[:id])
-    @receitas = @cozinha.recipe
+  end
+
+  def new
+    @cozinha = Cuisine.new
+  end
+
+  def create
+
+    cuisine_params = params.require(:cuisine).permit(:name)
+
+    @cozinha = Cuisine.new(cuisine_params)
+
+    if (@cozinha.save)
+      redirect_to cuisine_path(@cozinha.id)
+    else
+      render 'new'
+    end
+
   end
 end
--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,3 +1,4 @@
 class Cuisine < ApplicationRecord
-  has_many :recipe
+  has_many :recipes
+  validates :name, presence: true
 end
