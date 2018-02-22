6bfa0cbd7744e7895374c36472805b400337d314
Thais Kusuki <thais.kusuki@gmail.com>
2018-02-22 09:19:54 -0300

    Adding the 'new' action/view to register a recipe_type and passing the test 'User register recipe_type and must fill in name'

--- app/controllers/recipe_types_controller.rb
+++ app/controllers/recipe_types_controller.rb
@@ -2,4 +2,18 @@ class RecipeTypesController < ApplicationController
   def show
     @recipe_types = RecipeType.find(params[:id])
   end
+  def new
+    @recipe_type = RecipeType.new
+  end
+  def create
+    recipe_type_name = params[:recipe_type][:name]
+    @recipe_type = RecipeType.new(name: recipe_type_name)
+
+    if @recipe_type.save
+      redirect_to @recipe_type
+    else
+      flash[:error] = "Você deve informar o nome do tipo de receita"
+      render 'new'
+    end
+  end
 end
