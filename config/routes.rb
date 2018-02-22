92564af73127d94c2a6ee40346967d3d82fe1e60
Thais Kusuki <thais.kusuki@gmail.com>
2018-02-22 08:55:35 -0300

    Enabling only the routes that are used in the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  resources :recipes
-  resources :cuisines
-  resources :recipe_types
+  resources :recipes, only: [:show, :new, :create]
+  resources :cuisines, only: [:show, :new, :create]
+  resources :recipe_types, only: [:show, :new, :create]
 end
