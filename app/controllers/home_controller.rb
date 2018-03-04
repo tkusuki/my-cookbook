047419034a2ca38f77229f03c1417222279fa302
Thais Kusuki <thais.kusuki@gmail.com>
2018-03-03 23:25:04 -0300

    User view the latest recipes in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @recipes = Recipe.all
+    @recipes_latest = Recipe.order(created_at: :desc).limit(6)
     @cuisines = Cuisine.all
     @recipe_types = RecipeType.all
   end
